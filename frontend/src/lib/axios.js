e6a2004bfed7cdffb07e4f66a5fae7b39849f8ab
Ritik Mangal <107031511+RITIKMANGAL@users.noreply.github.com>
2025-02-10 11:18:54 +0530

    url changes

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL:"http://localhost:3000/api",
+  baseURL:"https://real-time-chat-application-txyb.onrender.com/api",
   withCredentials: true,
 });
